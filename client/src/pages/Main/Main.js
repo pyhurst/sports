bed64b2dab752e02b3801eba82be67e5b2d42a48
Philip Hurst <youngphil9@gmail.com>
2020-07-21 08:45:49 -0500

    added server and first few pages

---
+++ client/src/pages/Main/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Main.css";
+
+const Main = () => {
+  
+    return (
+        <>
+        <h1>Hi</h1>
+        </>
+    );
+  
+}
+
+export default Main;
---
+++ client/src/pages/NflTables/NflTables.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./NflTables.css";
+
+const NflTables = () => {
+  
+    return (
+        <>
+        <h1>Tables:</h1>
+        </>
+    );
+  
+}
+
+export default NflTables;
